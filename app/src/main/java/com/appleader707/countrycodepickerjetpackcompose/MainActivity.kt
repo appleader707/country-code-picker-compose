5f08c9c29f99a67c509a4651a43680ef6ee689c4
Amirhosein <ah.h.eghtedar@gmail.com>
2021-12-31 11:30:51 +0330

    This library is ready to use .

--- app/src/main/java/com/appleader707/countrycodepickerjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/appleader707/countrycodepickerjetpackcompose/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.appleader707.countrycodepickerjetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.appleader707.ccp.components.CountryCodePicker
+import com.appleader707.ccp.utils.getListCountries
 import com.appleader707.countrycodepickerjetpackcompose.ui.theme.CountryCodePickerJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,18 @@ class MainActivity : ComponentActivity() {
             CountryCodePickerJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                    val isOpen = remember { mutableStateOf(false) }
+
+                    Text(text = "Hello World")
+
+                    Button(onClick = { isOpen.value = true }) {
+                        Text(text = "Open CCP")
+                    }
+
+                    if (isOpen.value) {
+                        CCP()
+                    }
                 }
             }
         }
@@ -25,14 +41,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CCP() {
+    val countryCode = CountryCodePicker() // create a new Object
+    countryCode.CountryCodeDialog(
+        pickedCountry = {
+            Log.v("TAG", "country name is : ${it.countryName}")
+        },
+        defaultSelectedCountry = getListCountries().single { it.countryCode == "ir" },
+        dialogSearch = true,
+        dialogRounded = 22
+    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CountryCodePickerJetpackComposeTheme {
-        Greeting("Android")
-    }
-}
