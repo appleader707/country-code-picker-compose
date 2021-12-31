68cbddad62479d906ab44db4299f2f2c78909f64
Amirhosein <ah.h.eghtedar@gmail.com>
2021-12-31 11:31:46 +0330

    this library is ready to use

---
+++ CCP/src/main/java/com/appleader707/ccp/components/searchCountryList.kt
@@ -0,0 +1,13 @@
+package com.appleader707.ccp.components
+
+import com.appleader707.ccp.utils.CountryCode
+
+fun List<CountryCode>.searchCountryList(key: String): MutableList<CountryCode> {
+    val tempList = mutableListOf<CountryCode>()
+    this.forEach {
+        if (it.countryName.lowercase().contains(key.lowercase())) {
+            tempList.add(it)
+        }
+    }
+    return tempList
+}
